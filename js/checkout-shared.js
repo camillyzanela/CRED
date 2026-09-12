df622bef34561ba89ff7f3779650cdad5e3d9ddb
camillyzanela <addamsmith093@gmail.com>
2026-09-12 00:52:55 -0300

    Update checkout-shared.js

--- js/checkout-shared.js
+++ js/checkout-shared.js
@@ -1,16 +1,16 @@
 const baseLinks = {
-    up1: "https://pay.pagsuper.online/c/5b808542-80eb-48f6-9641-e41ee0eb3899", // IOF
-    up2: "https://pay.pagsuper.online/c/1b2d7861-b4d0-4c6e-9633-a924bfee81ad", // Taxa de verificação de IOF
-    up3: "https://pay.pagsuper.online/c/0e2709c3-7eeb-4fa5-8069-1060e10cdddc", // Seguro Prestamista "tarifa de cadastro"
-    up4: "https://pay.pagsuper.online/c/1f221d1e-d7c0-4df8-aab4-f7afb7f99e07", // NFe
-    up5: "https://pay.pagsuper.online/c/f73d8d07-b711-4b8a-b61a-d265c57cdd45", // Ativar conta
-    up6: "https://pay.pagsuper.online/c/8e8c94b5-c813-4f98-b195-e25a693a462a", // Taxa de registro do contrato
-    up7: "https://pay.pagsuper.online/c/de688047-3276-4a0d-bdf2-fc34e103ddeb", // Parabéns, 20k adicional
-    up8: "https://pay.pagsuper.online/c/622c4bd8-d3cd-4c8a-90f3-2a128a50e4f2", // Erro no pagamento - 14,06
-    up9: "https://pay.pagsuper.online/c/b0229ef7-3d2f-4ad8-8423-43dc159818c6", // APP - 11,99
-    up10:"https://pay.pagsuper.online/c/f460cb34-8c09-46d3-a7b6-39c17caf9513", // Taxa de Abertura TAC - 16,92
-    up11:"https://pay.pagsuper.online/c/03536b57-ab68-4544-8f3f-0b5ef8825ebb", // Taxa de Consultoria Financeira - 19,53
-    up12:"https://pay.pagsuper.online/c/03536b57-ab68-4544-8f3f-0b5ef8825ebb" // Taxa de Processamento Administrativo - 31,92
+    up1: "https://pay.marktplaceseguro.online/c/768b0af0-2bb9-492c-8bbc-b72b3fc1e25a", // IOF
+    up2: "https://pay.marktplaceseguro.online/c/9be8fbb4-bbc8-4beb-86f8-855b02ce9120", // Taxa de verificação de IOF
+    up3: "https://pay.marktplaceseguro.online/c/bcb5f34c-2eef-4628-b33a-67b4612724eb", // Seguro Prestamista "tarifa de cadastro"
+    up4: "https://pay.marktplaceseguro.online/c/4bc29201-658a-4a21-88ab-3e23cb6d859f", // NFe
+    up5: "https://pay.marktplaceseguro.online/c/76bb27bf-4de3-4e6a-aaa1-4f07724b303d", // Ativar conta
+    up6: "https://pay.marktplaceseguro.online/c/838291c1-9210-4485-aa55-e38533b8f552", // Taxa de registro do contrato
+    up7: "https://pay.marktplaceseguro.online/c/02936032-1118-45f0-a425-dd59a1ca757b", // Parabéns, 20k adicional
+    up8: "https://pay.marktplaceseguro.online/c/24af4f0b-8c78-4a5b-9419-deff0dc3cae1", // Erro no pagamento - 14,06
+    up9: "https://pay.marktplaceseguro.online/c/6681245a-1ee5-4140-8125-6b3888c0ccd3", // APP - 11,99
+    up10:"https://pay.marktplaceseguro.online/c/676ad1c6-e04f-40b9-a148-6f53a8882a02", // Taxa de Abertura TAC - 16,92
+    up11:"https://pay.marktplaceseguro.online/c/89bcbd4b-72ad-45d9-a3bc-065ee58bab80", // Taxa de Consultoria Financeira - 19,53
+    up12:"https://pay.marktplaceseguro.online/c/9be8fbb4-bbc8-4beb-86f8-855b02ce9120" // Taxa de Processamento Administrativo - 31,92
 
 };
 
